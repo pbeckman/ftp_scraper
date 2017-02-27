c7449061dd5c934c087480343609bb244ee942ee
pbeckman <pbeckman@uchicago.edu>
2017-02-26 21:26:23 -0600

    save readmes

--- save_readmes.py
+++ save_readmes.py
@@ -1,5 +1,6 @@
 import os
-from petrel_metadata_collector import get_globus_client, download_file
+import globus_sdk
+from petrel_metadata_collector import get_globus_client
 
 PETREL_ID = os.environ["PETREL_ID"]
 LOCAL_ID = os.environ["LOCAL_ID"]
@@ -7,11 +8,17 @@ TRANSFER_TOKEN = os.environ["TRANSFER_TOKEN"]
 
 
 def save_readmes(tc, endpoint_id, local_path, files, start_file_number):
-    for full_file_name in files[start_file_number:]:
+    for i in range(start_file_number, len(files)):
+        full_file_name = files[i]
         globus_path, file_name = full_file_name.strip().rsplit("/", 1)
         globus_path += "/"
         if "readme" in file_name.lower():
-            download_file(tc, endpoint_id, globus_path, file_name, local_path)
+            print("downloading file {}".format(globus_path + file_name))
+            tdata = globus_sdk.TransferData(tc, endpoint_id, LOCAL_ID)
+            tdata.add_item(globus_path + file_name, local_path + file_name + str(i))
+
+            tc.submit_transfer(tdata)
+
 
 tc = get_globus_client()
 
