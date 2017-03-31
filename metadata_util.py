ee32ae5594cbd5d2f2fd86b3bf9037ada6dfe39b
pbeckman <pbeckman@uchicago.edu>
2017-03-31 11:03:33 -0500

    stashing before big changes

--- metadata_util.py
+++ metadata_util.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import numpy
+import os
 from netCDF4 import Dataset
 from decimal import Decimal
 from operator import itemgetter
@@ -18,7 +19,7 @@ def get_metadata(file_name, path):
     """Create metadata JSON from file.
 
         :param file_name: (str) file name
-        :param path: (str) path to file
+        :param path: (str) absolute or relative path to file
         :returns: (dict) metadata dictionary"""
 
     with open(path + file_name, 'rU') as file_handle:
@@ -29,25 +30,24 @@ def get_metadata(file_name, path):
 
         try:
             if extension in ["csv", "txt"]:
-                metadata = get_columnar_metadata(file_handle, extension)
+                metadata = get_columnar_metadata(file_handle)
         except ExtractionError:
             # not a columnar file
             pass
 
         if extension == "nc":
-            metadata = get_netcdf_metadata(file_name, path)
+            metadata = get_netcdf_metadata(file_handle)
 
     return metadata
 
 
-def get_netcdf_metadata(file_name, path):
+def get_netcdf_metadata(file_handle):
     """Create netcdf metadata JSON from file.
 
-        :param file_name: (str) file name
-        :param path: (str) path to file
+        :param file_handle: (str) file
         :returns: (dict) metadata dictionary"""
 
-    dataset = Dataset(path + file_name)
+    dataset = Dataset(os.path.realpath(file_handle.name))
     metadata = {
         "file_format": dataset.file_format,
     }
@@ -113,27 +113,27 @@ class NumpyDecoder(json.JSONEncoder):
             return super(NumpyDecoder, self).default(obj)
 
 
-def get_columnar_metadata(file_handle, extension):
-    """Get all header fields from file, return None if none can be retrieved.
+def get_columnar_metadata(file_handle):
+    """Get metadata from column-formatted file.
 
         :param file_handle: (file) open file
-        :param extension: (str) file extension used to determine csv.reader parameters
         :returns: (dict) ascertained metadata
         :raises: (ExtractionError) if the file cannot be read as a columnar file"""
 
-    # TODO: determine if whitespace separation for non-csv, and comma separated excel dialect for csv are effective
+    extension = file_handle.name.split('.', 1)[1] if '.' in file_handle.name else "no extension"
 
-    # choose csv.reader parameters based on file type - if not csv, try space-delimited
-    if extension == "csv":
+    # choose csv.reader parameters based on file type - if not csv, use space-delimited
+    if extension in ["csv", "exc.csv"]:
         reader = csv.reader(file_handle, skipinitialspace=True)
     else:
         reader = SpaceDelimitedReader(file_handle)
 
-    # base dictionary for all the metadata to go in
-    metadata = {}
+    # base dictionary in which to store all the metadata
+    metadata = {"columns": {}}
 
     headers = []
-    header_types = []
+    col_types = []
+    col_aliases = []
     num_value_rows = 0
     num_header_rows = 0
     # used to check if all rows are the same length, if not, this is not a valid columnar file
@@ -148,6 +148,9 @@ def get_columnar_metadata(file_handle, extension):
         # update row length for next check
         row_length = len(row)
 
+        # make column aliases so that we can create aggregates even for unlabelled columns
+        col_aliases = ["__{}__".format(i) for i in range(0, row_length)]
+
         # if the row is a header row, add all its fields to the headers list
         if is_header_row(row):
             num_header_rows += 1
@@ -155,110 +158,118 @@ def get_columnar_metadata(file_handle, extension):
                 if header != "":
                     headers.append(header)
 
-        # don't return column aggregate data for files with multiple header rows
-        elif num_header_rows == 1:
+        else:
+            # set the column aliases to the headers if they are 1-to-1 (single header row)
+            if num_header_rows == 1:
+                col_aliases = headers
+
             num_value_rows += 1
 
             # type check the first row to decide which aggregates to use
             if num_value_rows == 1:
-                header_types = ["num" if is_number(field) else "str" for field in row]
+                col_types = ["num" if is_number(field) else "str" for field in row]
 
-            add_row_to_aggregates(metadata, row, headers, header_types, num_value_rows == 1)
+            add_row_to_aggregates(metadata, row, col_aliases, col_types, num_value_rows == 1)
 
     # add header list to metadata
     if len(headers) > 0:
         metadata["headers"] = headers
 
-    # if there is a single header row and therefore were aggregates made, add the averages
-    if num_header_rows == 1:
-        add_final_aggregates(metadata, headers, header_types, num_value_rows)
+    add_final_aggregates(metadata, col_aliases, col_types, num_value_rows)
 
     return metadata
 
 
-def add_row_to_aggregates(metadata, row, headers, header_types, is_first_value_row):
+def add_row_to_aggregates(metadata, row, col_aliases, col_types, is_first_value_row):
     """Adds row data to aggregates.
 
         :param metadata: (dict) metadata dictionary to add to
         :param row: (list(str)) row of strings to add
-        :param headers: (list(str)) list of headers
-        :param header_types: (list("num" | "str")) list of header types
+        :param col_aliases: (list(str)) list of headers
+        :param col_types: (list("num" | "str")) list of header types
         :param is_first_value_row: (bool) whether this is the first value row, so we need to initialize
         the necessary aggregate dictionary in the metadata"""
 
     for i in range(0, len(row)):
         value = row[i]
-        header = headers[i]
-        header_type = header_types[i]
+        col_alias = col_aliases[i]
+        col_type = col_types[i]
 
         if is_first_value_row:
-            metadata[header] = {}
-            metadata[header]["frequencies"] = {str(value): 1}
+            metadata["columns"][col_alias] = {}
+            metadata["columns"][col_alias]["frequencies"] = {str(value): 1}
         else:
-            if str(value) in metadata[header]["frequencies"].keys():
-                metadata[header]["frequencies"][str(value)] += 1
+            if str(value) in metadata["columns"][col_alias]["frequencies"].keys():
+                metadata["columns"][col_alias]["frequencies"][str(value)] += 1
             else:
-                metadata[header]["frequencies"][str(value)] = 1
+                metadata["columns"][col_alias]["frequencies"][str(value)] = 1
 
-        if header_type == "num":
+        if col_type == "num":
             # cast the field to a number to do numerical aggregates
             value = float(value)
 
             # start off the metadata if this is the first row of values
             if is_first_value_row:
-                metadata[header]["min"] = [float("inf"), float("inf"), float("inf")]
-                metadata[header]["max"] = [None, None, None]
-                metadata[header]["total"] = value
+                metadata["columns"][col_alias]["min"] = [float("inf"), float("inf"), float("inf")]
+                metadata["columns"][col_alias]["max"] = [None, None, None]
+                metadata["columns"][col_alias]["total"] = value
 
             # add row data to existing aggregates
             else:
-                if value < metadata[header]["min"][0]:
-                    metadata[header]["min"][1:2] = metadata[header]["min"][0:1]
-                    metadata[header]["min"][0] = value
-                elif value < metadata[header]["min"][1] and value != metadata[header]["min"][0]:
-                    metadata[header]["min"][2] = metadata[header]["min"][1]
-                    metadata[header]["min"][1] = value
-                elif value < metadata[header]["min"][2] and value not in metadata[header]["min"][:2]:
-                    metadata[header]["min"][2] = value
-                if value > metadata[header]["max"][0]:
-                    metadata[header]["max"][1:2] = metadata[header]["max"][0:1]
-                    metadata[header]["max"][0] = value
-                elif value > metadata[header]["max"][1] and value != metadata[header]["max"][0]:
-                    metadata[header]["max"][2] = metadata[header]["max"][1]
-                    metadata[header]["max"][1] = value
-                elif value > metadata[header]["max"][2] and value not in metadata[header]["max"][:2]:
-                    metadata[header]["max"][2] = value
-                metadata[header]["total"] += value
+                if value < metadata["columns"][col_alias]["min"][0]:
+                    metadata["columns"][col_alias]["min"][1:2] = metadata["columns"][col_alias]["min"][0:1]
+                    metadata["columns"][col_alias]["min"][0] = value
+                elif value < metadata["columns"][col_alias]["min"][1] \
+                        and value != metadata["columns"][col_alias]["min"][0]:
+                    metadata["columns"][col_alias]["min"][2] = metadata["columns"][col_alias]["min"][1]
+                    metadata["columns"][col_alias]["min"][1] = value
+                elif value < metadata["columns"][col_alias]["min"][2] \
+                        and value not in metadata["columns"][col_alias]["min"][:2]:
+                    metadata["columns"][col_alias]["min"][2] = value
+                if value > metadata["columns"][col_alias]["max"][0]:
+                    metadata["columns"][col_alias]["max"][1:2] = metadata["columns"][col_alias]["max"][0:1]
+                    metadata["columns"][col_alias]["max"][0] = value
+                elif value > metadata["columns"][col_alias]["max"][1] \
+                        and value != metadata["columns"][col_alias]["max"][0]:
+                    metadata["columns"][col_alias]["max"][2] = metadata["columns"][col_alias]["max"][1]
+                    metadata["columns"][col_alias]["max"][1] = value
+                elif value > metadata["columns"][col_alias]["max"][2] \
+                        and value not in metadata["columns"][col_alias]["max"][:2]:
+                    metadata["columns"][col_alias]["max"][2] = value
+                metadata["columns"][col_alias]["total"] += value
 
-        elif header_type == "str":
+        elif col_type == "str":
             # TODO: add string-specific field aggregates?
             pass
 
 
-def add_final_aggregates(metadata, headers, header_types, num_value_rows):
+def add_final_aggregates(metadata, col_aliases, col_types, num_value_rows):
     """Adds row data to aggregates.
 
         :param metadata: (dict) metadata dictionary to add to
-        :param headers: (list(str)) list of headers
-        :param header_types: (list("num" | "str")) list of header types
+        :param col_aliases: (list(str)) list of headers
+        :param col_types: (list("num" | "str")) list of header types
         :param num_value_rows: (int) number of value rows"""
 
     # calculate averages for numerical columns if aggregates were taken,
     # (which only happens when there is a single row of headers)
-    for i in range(0, len(headers)):
-        header = headers[i]
-        metadata[header]["mode"] = max(metadata[header]["frequencies"].iteritems(), key=itemgetter(1))[0]
-        metadata[header].pop("frequencies")
+    for i in range(0, len(col_aliases)):
+        col_alias = col_aliases[i]
+        metadata["columns"][col_alias]["mode"] = max(metadata["columns"][col_alias]["frequencies"].iteritems(),
+                                                     key=itemgetter(1))[0]
+        metadata["columns"][col_alias].pop("frequencies")
 
-        if header_types[i] == "num":
-            metadata[header]["max"] = [val for val in metadata[header]["max"] if val is not None]
-            metadata[header]["min"] = [val for val in metadata[header]["min"] if val != float("inf")]
+        if col_types[i] == "num":
+            metadata["columns"][col_alias]["max"] = [val for val in metadata["columns"][col_alias]["max"]
+                                                     if val is not None]
+            metadata["columns"][col_alias]["min"] = [val for val in metadata["columns"][col_alias]["min"]
+                                                     if val != float("inf")]
 
-            metadata[header]["avg"] = round(
-                metadata[header]["total"] / num_value_rows,
-                max_precision([metadata[header]["min"][0], metadata[header]["max"][0]])
-            ) if len(metadata[header]["min"]) > 0 else None
-            metadata[header].pop("total")
+            metadata["columns"][col_alias]["avg"] = round(
+                metadata["columns"][col_alias]["total"] / num_value_rows,
+                max_precision([metadata["columns"][col_alias]["min"][0], metadata["columns"][col_alias]["max"][0]])
+            ) if len(metadata["columns"][col_alias]["min"]) > 0 else None
+            metadata["columns"][col_alias].pop("total")
 
 
 def max_precision(nums):
