bfa540dea8ac7329deadae61bf0e498d3683949e
pbeckman <pbeckman@uchicago.edu>
2017-04-25 08:58:27 -0500

    restart

--- petrel_metadata_collector.py
+++ petrel_metadata_collector.py
@@ -216,7 +216,7 @@ t0 = time.time()
 with open(os.path.expanduser("~/Documents/paul/metadata/pub8_list.txt"), "r") as file_list:
     with open(os.path.expanduser("~/Documents/paul/metadata/metadata.txt"), "a") as metadata_file:
         # metadata_file.write('{"files":[')
-        classify_files(tc, PETREL_ID, file_list.readlines(), 8607,
+        classify_files(tc, PETREL_ID, file_list.readlines(), 10351,
                        os.path.expanduser("~/Documents/paul/metadata/download/"),
                        metadata_file,
                        os.path.expanduser("~/Documents/paul/metadata/restart.csv"))
